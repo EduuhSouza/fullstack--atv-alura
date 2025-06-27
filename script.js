20aa7dc74f184186c5f015cfea869f7a0b632c38
EduuhSouza <146948069+EduuhSouza@users.noreply.github.com>
2025-06-27 16:16:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,79 @@
+// let nome = "Eduarda";
+
+// alert("Bem-vinda " + nome);
+// console.log("Bem-vinda " + nome);
+
+// let resposta 
+// let resposta = prompt("Qual é a linguagem de programação que você mais gosta?"); 
+// console.log(resposta);
+
+// alert("Que bom que você gosta de " + resposta);
+
+
+
+//6 - VALORES
+
+// let valor1 = parseInt(prompt("Digite um valor"));
+// let valor2 = parseInt(prompt("Digite o segundo valor"));
+// let resultado = (valor1 + valor2);
+
+// alert(`A soma de ${valor1} + ${valor2} é igual a ${resultado}`);
+// console.log(`A soma de ${valor1} + ${valor2} é igual a ${resultado}`);
+
+// 7 - VALORES SUBTRAINDO
+
+// let valor1 = parseInt(prompt("Digite um valor"));
+// let valor2 = parseInt(prompt("Digite o segundo valor"));
+// let resultado = (valor1 - valor2);
+
+// alert(`A diferença entre ${valor1} e ${valor2} é ${resultado}`);
+// console.log(`A diferença entre ${valor1} e ${valor2} é ${resultado}`);
+
+// 7 - MENOR OU MAIOR DE IDADE
+
+// let idadeUser = prompt("Digite a sua idade");
+
+// if(idadeUser >= 18){
+//     alert("Você é maior de idade");
+//     console.log("Você é maior de idade");
+// } else{
+//     alert("Você é menor de idade");
+//     console.log("Você é menor de idade");
+// }
+
+
+// 8 - VERIFIQUE O NUMERO SE FOR NEAGTIVO, POSITIVO OU ZERO
+
+// let numero = prompt("Dgite um número");
+
+// if( numero < 0){
+//     alert("Este número é negativo");
+// } else {
+//     if( numero == 0){
+//         alert("Este nummero é zero");
+//     } else {
+//         alert("Este numero é positivo")
+//     }
+// }
+
+
+// 9 - LOOP While 
+//  alert('vamos contar ate 10');
+
+//  let contador = 0;
+
+//  while(contador <= 10){
+//      console.log(contador);
+//     contador++;
+//  }
+
+// 10 - NOTA
+// let nota = prompt("Digite sua nota");
+
+// if( nota >=7 ){
+//     alert("Sua nota é " + nota + " você foi Aprovado");
+// } else{ 
+    // alert("Sua nota é " + nota + " você foi Reprovado");
+// };
+
+// 11 - NUMERO ALEATORIO
